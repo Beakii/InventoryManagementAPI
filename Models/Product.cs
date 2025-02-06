a68e1d167512e740f63fd15d8a109325a5f8cc0f
James <auminist3@gmail.com>
2025-02-06 14:00:03 +1300

    Added SupplierName property to the Product.cs so that when supplying the json to the endpoint, you can just supply the Supplier name instead of the entire supplier object.

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,7 +5,9 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; }
     public string SKU { get; set; }
-    public int Price { get; set; }
+    public float Price { get; set; }
     public int Stock { get; set; }
-    public Supplier Supplier { get; set; }
+    
+    public string SupplierName { get; set; }
+    public Supplier? Supplier { get; set; }
 }
